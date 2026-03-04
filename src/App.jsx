4b9aa1fbf06bb28f6493f15f89b5c2fe1956f8b9
solomon-israel <solomonisraelayomide630@gmail.com>
2026-03-04 19:38:06 +0100

    fix: added default path and renamed page title

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,13 @@ import { BrowserRouter, Route, Routes } from "react-router";
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
 import Forgotpassword from "./pages/Forgotpassword";
+import Onboarding from "./pages/Onboarding";
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path="/" element={<Onboarding />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Signup />} />
         <Route path="/forget-password" element={<Forgotpassword />} />
