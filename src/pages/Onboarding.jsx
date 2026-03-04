77dbe3b78421695a422dff1013068a1b8ff47d4e
solomon-israel <solomonisraelayomide630@gmail.com>
2026-03-04 19:53:32 +0100

    chore: added slide in right animation to the onboarding page

--- src/pages/Onboarding.jsx
+++ src/pages/Onboarding.jsx
@@ -1,10 +1,17 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { FaMessage } from "react-icons/fa6";
+import { useNavigate } from "react-router";
 
 export default function Onboarding() {
+  const navigate = useNavigate();
+  useEffect(() => {
+    setTimeout(() => {
+      navigate("/login");
+    }, 1500);
+  }, []);
   return (
     <div className="w-full flex items-center justify-center bg-blue-700 min-h-screen">
-      <FaMessage className="size-15 text-blue-900" />
+      <FaMessage className="slideInRight size-15 text-blue-900" />
     </div>
   );
 }
