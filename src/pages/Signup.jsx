436aea7885b306b2e50e1542a5cd49e5810b6162
solomon-israel <solomonisraelayomide630@gmail.com>
2026-03-06 18:22:06 +0100

    fix: amended the signup page

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -4,38 +4,71 @@ import { Link } from "react-router";
 
 export default function Signup() {
   return (
-    <div className="h-screen w-full p-4 ">
-      <div className="space-y-4">
-        <header className="mb-6">
-          <span>Create account</span>
-        </header>
-
+    <div className="h-screen w-full px-4 py-10">
+      <div className="text-center mb-8">
+        <h1 className="font-bold text-3xl mb-2">Create account</h1>
+        <p className="text-gray-500 text-sm">
+          Join us today at scale chat and connect with your like minds
+        </p>
+      </div>
+      <form className="space-y-4 mb-6">
         <div className="flex flex-col space-y-2">
           <span>First name</span>
-          <input className="border rounded outline-0 p-2" type="text" />
-          <p className="text-zinc-400 ">Enter your first name</p>
+          <input
+            type="text"
+            className="slideInRight border w-full h-11 rounded-lg px-2 outline-none border-gray-400"
+          />
+          <p className="text-zinc-400 text-sm">Enter your first name</p>
         </div>
 
         <div className="flex flex-col space-y-2">
           <span>Last name</span>
-          <input className="border rounded outline-0 p-2" type="text" />
-          <p className="text-zinc-400 ">Enter your last name</p>
+          <input
+            type="text"
+            className="slideInRight border w-full h-11 rounded-lg px-2 outline-none border-gray-400"
+          />
+          <p className="text-zinc-400 text-sm">Enter your last name</p>
         </div>
 
         <div className="flex flex-col space-y-2">
           <span>Username or email address</span>
-          <input className="border rounded outline-0 p-2" type="text" />
-          <p className="text-zinc-400 ">Enter your username or email address</p>
+          <input
+            type="text"
+            className="slideInRight border w-full h-11 rounded-lg px-2 outline-none border-gray-400"
+          />
+          <p className="text-zinc-400 text-sm">
+            Enter your username or email address
+          </p>
         </div>
 
         <div className="flex flex-col space-y-2">
           <span>Password</span>
-          <input className="border rounded outline-0 p-2" type="text" />
-          <p className="text-zinc-400 ">Enter your password</p>
+          <input
+            type="text"
+            className="slideInRight border w-full h-11 rounded-lg px-2 outline-none border-gray-400"
+          />
+          <p className="text-zinc-400 text-sm">Enter your password</p>
         </div>
 
-        <Link className="text-blue-500" to="/forgot-password">Forgot password?</Link>
-      </div>
+        <div className="flex flex-col space-y-2">
+          <span>Mobile number</span>
+          <input
+            type="text"
+            className="slideInRight border w-full h-11 rounded-lg px-2 outline-none border-gray-400"
+          />
+          <p className="text-zinc-400 text-sm">Enter your mobile number</p>
+        </div>
+
+        <button className="border w-full p-2 rounded-lg bg-blue-700 text-white">
+          Login
+        </button>
+      </form>
+      <p className="text-sm text-center font-normal">
+        Already have an account?
+        <Link className="text-blue-700 ml-2 font-semibold" to="/login">
+          Signin
+        </Link>
+      </p>
     </div>
   );
 }
