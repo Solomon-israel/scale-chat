c1939ae6327796bb0eca6d3737f2ad6dfe20a108
solomon-israel <solomonisraelayomide630@gmail.com>
2026-03-06 19:36:50 +0100

    created a local storage and made some adjustment

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,20 @@ import Login from "./pages/Login";
 import Signup from "./pages/Signup";
 import Forgotpassword from "./pages/Forgotpassword";
 import Onboarding from "./pages/Onboarding";
+import { ToastContainer } from "react-toastify";
 
 export default function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Onboarding />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Signup />} />
-        <Route path="/forgot-password" element={<Forgotpassword />} />
-      </Routes>
-    </BrowserRouter>
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Onboarding />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Signup />} />
+          <Route path="/forgot-password" element={<Forgotpassword />} />
+        </Routes>
+      </BrowserRouter>
+      <ToastContainer position="top-center" />
+    </>
   );
 }
--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -1,8 +1,36 @@
-import React from "react";
-import { FaMessage } from "react-icons/fa6";
-import { Link } from "react-router";
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router";
+import { toast } from "react-toastify";
 
 export default function Signup() {
+  const navigate = useNavigate();
+
+  const [formData, setFormData] = useState({
+    firstname: "",
+    lastname: "",
+    password: "",
+    email: "",
+    number: "",
+  });
+
+  function handleOnChange(e) {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    const users = JSON.parse(localStorage.getItem("users")) || [];
+    const { firstname, lastname, email, password, number } = formData;
+
+    if (!firstname || !lastname || !email || !password || !number)
+      return toast.warning("All fields required");
+    // create localstorage
+    localStorage.setItem("users", JSON.stringify([...users, formData]));
+    toast.success("Account created succesfully!");
+    navigate("/login");
+  }
+
   return (
     <div className="h-screen w-full px-4 py-10">
       <div className="text-center mb-8">
@@ -11,56 +39,67 @@ export default function Signup() {
           Join us today at scale chat and connect with your like minds
         </p>
       </div>
-      <form className="space-y-4 mb-6">
-        <div className="flex flex-col space-y-2">
+      <form className="space-y-4 mb-6" onSubmit={handleSubmit}>
+        <div className="flex flex-col space-y-2 slideInRight">
           <span>First name</span>
           <input
             type="text"
-            className="slideInRight border w-full h-11 rounded-lg px-2 outline-none border-gray-400"
+            className=" border w-full h-11 rounded-lg px-2 outline-none border-gray-400"
+            name="firstname"
+            value={formData.firstanme}
+            onChange={handleOnChange}
           />
           <p className="text-zinc-400 text-sm">Enter your first name</p>
         </div>
 
-        <div className="flex flex-col space-y-2">
+        <div className="flex flex-col space-y-2 slideInRight">
           <span>Last name</span>
           <input
             type="text"
-            className="slideInRight border w-full h-11 rounded-lg px-2 outline-none border-gray-400"
+            name="lastname"
+            className=" border w-full h-11 rounded-lg px-2 outline-none border-gray-400"
+            onChange={handleOnChange}
           />
           <p className="text-zinc-400 text-sm">Enter your last name</p>
         </div>
 
-        <div className="flex flex-col space-y-2">
-          <span>Username or email address</span>
+        <div className="flex flex-col space-y-2 slideInRight">
+          <span>Email address</span>
           <input
-            type="text"
-            className="slideInRight border w-full h-11 rounded-lg px-2 outline-none border-gray-400"
+            type="email"
+            className=" border w-full h-11 rounded-lg px-2 outline-none border-gray-400"
+            onChange={handleOnChange}
+            name="email"
           />
           <p className="text-zinc-400 text-sm">
             Enter your username or email address
           </p>
         </div>
 
-        <div className="flex flex-col space-y-2">
+        <div className="flex flex-col space-y-2 slideInRight">
           <span>Password</span>
           <input
-            type="text"
-            className="slideInRight border w-full h-11 rounded-lg px-2 outline-none border-gray-400"
+            type="password"
+            className=" border w-full h-11 rounded-lg px-2 outline-none border-gray-400"
+            onChange={handleOnChange}
+            name="password"
           />
           <p className="text-zinc-400 text-sm">Enter your password</p>
         </div>
 
-        <div className="flex flex-col space-y-2">
+        <div className="flex flex-col space-y-2 slideInRight">
           <span>Mobile number</span>
           <input
-            type="text"
-            className="slideInRight border w-full h-11 rounded-lg px-2 outline-none border-gray-400"
+            type="tel"
+            className=" border w-full h-11 rounded-lg px-2 outline-none border-gray-400"
+            onChange={handleOnChange}
+            name="number"
           />
           <p className="text-zinc-400 text-sm">Enter your mobile number</p>
         </div>
 
         <button className="border w-full p-2 rounded-lg bg-blue-700 text-white">
-          Login
+          Create account
         </button>
       </form>
       <p className="text-sm text-center font-normal">
